1d690828483efed60006a8919405d4b02cdeb72f
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2024-09-12 22:56:51 +0530

    Time: 4 ms (96.08%), Space: 48.2 MB (5.27%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,32 +1,73 @@
 //Using extra Space
+// class MinStack {
+
+//     Stack<Integer> stack;
+//     Stack<Integer> minStack;
+//     public MinStack() {
+//         stack = new Stack();
+//         minStack = new Stack();
+//     }
+    
+//     public void push(int val) {
+//         stack.push(val);
+//         if(minStack.isEmpty() || minStack.peek() >= val)
+//             minStack.push(val);  
+//     }
+    
+//     public void pop() {
+//         int val = stack.pop();
+//         if(minStack.peek() == val)
+//             minStack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+//     }
+    
+//     public int getMin() {
+//         return minStack.peek();
+//     }
+// }
+
 
 class MinStack {
 
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+    Stack<Long> stack;
+    Long minE=Long.MAX_VALUE;
     public MinStack() {
         stack = new Stack();
-        minStack = new Stack();
     }
     
-    public void push(int val) {
-        stack.push(val);
-        if(minStack.isEmpty() || minStack.peek() >= val)
-            minStack.push(val);  
+    public void push(int valv) {
+        
+       long val =  Long.valueOf(valv);
+        if(stack.isEmpty()){
+            stack.push(val);
+            minE = val;
+        }else if(val >= minE){
+            stack.push(val);
+        }else{
+            long flag = 2*val - minE;
+            stack.push(flag);
+            minE = val;
+        }     
     }
     
     public void pop() {
-        int val = stack.pop();
-        if(minStack.peek() == val)
-            minStack.pop();
+        long p = stack.pop();
+        if(p < minE){
+            minE = 2*minE -p;
+        }
     }
     
     public int top() {
-        return stack.peek();
+        if(stack.peek() < minE)
+            return minE.intValue();
+        return stack.peek().intValue();
     }
     
     public int getMin() {
-        return minStack.peek();
+        return minE.intValue();
     }
 }
 
