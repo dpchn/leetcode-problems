ab45de127a7685926cc5df6e20d3264bfeead972
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2024-09-12 08:40:41 +0530

    Time: 32 ms (44.65%), Space: 53.7 MB (87.17%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,21 +1,46 @@
 class StockSpanner {
 
+    List<Integer> prices;
+    Stack<Integer> stack;
     public StockSpanner() {
-        
+        prices = new ArrayList();
+        stack = new Stack();
     }
     
-    List<Integer> prices = new ArrayList();
+    
+    // public int next(int price) {
+    //     prices.add(price);
+    //     int pge=-1;
+    //     if(stack.size()==0){
+    //         pge = -1;
+    //     }else if(prices.get(stack.peek()) > price){
+    //         pge = stack.peek();
+    //     }else {
+    //         while(stack.size() > 0 && prices.get(stack.peek()) <= price)
+    //             stack.pop();
+    //         if(stack.size()==0){
+    //             pge = -1;
+    //         }else{
+    //             pge = stack.peek();
+    //         }
+    //     }
+    //     stack.push(prices.size()-1);
+    //     return prices.size()-1 - pge;
+    // }
+
     public int next(int price) {
         prices.add(price);
-        int len = prices.size(); 
-        return leftGreater(price);
-    }
-
-    int leftGreater(int price){
-        int i = prices.size()-1;
-        while(i >=0 && prices.get(i)<= price)
-            i--;
-        return prices.size() - i-1;
+        int pge=-1;
+        while(stack.size() > 0 && prices.get(stack.peek()) <= price)
+            stack.pop();
+        
+        if(stack.size()==0){
+            pge = -1;
+        }else{
+            pge = stack.peek();
+        }
+        stack.push(prices.size()-1);
+        return prices.size()-1 - pge;
     }
 }
 
