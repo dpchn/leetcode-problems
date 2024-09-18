e498dbff383726a883ffa8859f8493b7c314ea1e
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2024-09-18 23:22:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (73.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,42 +1,41 @@
 class Solution {
     // public int findPeakElement(int[] nums) {
-    //     int len = nums.length;
-    //     int low =0, high = len-1;
-    //     while(low <high){
-    //         int mid = low + (high-low)/2;
-    //         if(mid>0 && mid < len-1 ){
-    //             if(nums[mid]> nums[mid-1] && nums[mid]> nums[mid+1])
-    //                 return mid;
-    //             else if(nums[mid]< nums[mid+1] ){
-    //                 low = mid+1;
-    //             }else {
-    //                 high = mid-1;
-    //             }
-    //         }else if(mid==0 && nums[mid] > nums[mid+1])
-    //             return mid;
-    //         else if(mid==len-1 && nums[mid] > nums[mid-1]){
-    //             return mid;
-    //         }else if(len==2){
-    //             if(nums[0]> nums[1])
-    //                 return 0;
-    //             return 1;
-    //         }
-    //     }
-    //     return low;
+    // int len = nums.length;
+    // int low =0, high = len-1;
+    // while(low <high){
+    // int mid = low + (high-low)/2;
+    // if(mid>0 && mid < len-1 ){
+    // if(nums[mid]> nums[mid-1] && nums[mid]> nums[mid+1])
+    // return mid;
+    // else if(nums[mid]< nums[mid+1] ){
+    // low = mid+1;
+    // }else {
+    // high = mid-1;
     // }
-
+    // }else if(mid==0 && nums[mid] > nums[mid+1])
+    // return mid;
+    // else if(mid==len-1 && nums[mid] > nums[mid-1]){
+    // return mid;
+    // }else if(len==2){
+    // if(nums[0]> nums[1])
+    // return 0;
+    // return 1;
+    // }
+    // }
+    // return low;
+    // }
 
     public int findPeakElement(int[] nums) {
         int len = nums.length;
-        int low =0, high = len-1;
-        while(low <high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] < nums[mid+1])
-                low = mid+1;
-            else 
+        int low = 0, high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1])
+                low = mid + 1;
+            else
                 high = mid;
         }
-        return high;
+        return low;
     }
-    
+
 }
