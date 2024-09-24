ab27056490cde2d5bf0aeeb76da807e2f8527d3e
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2024-09-24 16:49:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (12.2%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -6,18 +6,19 @@ class Solution {
     }
 
     void findSubSet(int[] nums, int start, int len, List<List<Integer>> result, List<Integer> op){
-        if(len-start==0){
+        if(len-start==0){ //input empty then getting  output
             result.add(op);
             return;
         }
 
         List<Integer> op1= op;
         List<Integer> op2= addElement(op, nums[start]);
-        start = start+1;
-        findSubSet(nums, start,len, result, op1 );
-        findSubSet(nums, start,len, result, op2 );
+        start = start+1; //Keep reducing input
+        findSubSet(nums, start,len, result, op1 ); //ith index element not choosen
+        findSubSet(nums, start,len, result, op2 ); //ith index element  choosen
     }
 
+    //Adding current element in output
     List<Integer>  addElement(List<Integer> op, int n){
         List<Integer> res = new ArrayList();
         res.addAll(op);
