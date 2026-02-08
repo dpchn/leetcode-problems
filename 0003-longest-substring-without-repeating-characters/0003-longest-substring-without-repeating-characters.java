4a0e31bea5a64bb227cf3b4d4d01c0fe7d569f4c
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-02-08 12:22:31 +0530

    Time: 2 ms (97.57%), Space: 44.8 MB (93.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,46 +1,18 @@
 class Solution {
-    // public int lengthOfLongestSubstring(String s) {
-    //     int freq[] = new int[26];
-    //     int len = s.length();
-    //     int i=0,j=0;
-    //     int max=0;
-    //     if(s.trim().length()<2){
-    //         if(s.length()==0)
-    //             return 0;
-    //         return 1;
-    //     }
-    //     while(j<len){
-    //         char ch = s.charAt(j);
-    //         if()
-    //         freq[ch-'a']++;
-    //         while(freq[ch-'a']>1){
-    //             char c= s.charAt(i++);
-    //             freq[c-'a']--;
-    //         }
-
-    //         max = Math.max(max, j-i+1);
-    //         j++;
-    //     }
-    //     return max;
-    // }
-
-
-
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> freq = new HashMap();
-        int len = s.length();
-        int i=0,j=0;
         int max=0;
-        while(j<len){
-            char ch = s.charAt(j);
-            freq.put(ch, freq.getOrDefault(ch, 0)+1);
-            while(freq.get(ch)>1){
-                char c= s.charAt(i++);
-                freq.put(c, freq.get(c)-1);
+        int left =0, right =0, len = s.length();
+        int counterArr[] = new int [128];
+        while(right < len){
+            char ch = s.charAt(right);
+            while(left < right && counterArr[ch] > 0){
+                counterArr[s.charAt(left)]--;
+                left++;
             }
 
-            max = Math.max(max, j-i+1);
-            j++;
+            counterArr[ch]++;
+            max = Math.max(max, right - left +1);
+            right++;
         }
         return max;
     }
