e8adf9a6a1812976d94d0af07480f433dfba20af
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-03-02 22:57:54 +0530

    Time: 6 ms (96.24%), Space: 46.5 MB (91.34%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,29 +1,51 @@
 class Solution {
+    // public String removeDuplicates(String s) {
+    //     Stack<Character> stack = new Stack();
+
+    //     for(int i = s.length()-1; i>=0; i--){
+    //         char c = s.charAt(i);
+    //         if(stack.isEmpty()){
+    //             stack.push(c);
+    //         }else{
+    //             boolean needToAdd = false;
+    //             while(!stack.isEmpty() && stack.peek() == c){
+    //                 stack.pop();
+    //                 needToAdd = true;
+    //             }
+
+    //             if(!needToAdd){
+    //                 stack.push(c);
+    //             }
+    //         }
+    //     }
+
+    //     StringBuilder sb = new StringBuilder();
+    //     while(!stack.isEmpty()){
+    //         sb.append(stack.pop());
+    //     }
+
+    //     return sb.toString();
+    // }
+
+
+
     public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack();
+        char[] res = s.toCharArray();
+        int i = 0; // 'i' acts as the stack pointer (index of next available spot)
 
-        for(int i = s.length()-1; i>=0; i--){
-            char c = s.charAt(i);
-            if(stack.isEmpty()){
-                stack.push(c);
-            }else{
-                boolean needToAdd = false;
-                while(!stack.isEmpty() && stack.peek() == c){
-                    stack.pop();
-                    needToAdd = true;
-                }
-
-                if(!needToAdd){
-                    stack.push(c);
-                }
+        for (int j = 0; j < s.length(); j++) {
+            // res[j] is the current character we are looking at
+            // if 'i > 0', we have elements in our "stack"
+            // if 'res[i - 1] == res[j]', we found an adjacent duplicate
+            if (i > 0 && res[i - 1] == res[j]) {
+                i--; // "Pop" from stack by moving pointer back
+            } else {
+                res[i] = res[j]; // "Push" current char to the stack position
+                i++; // Move pointer forward
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-
-        return sb.toString();
+        
+        // The characters from index 0 to i represent the final string
+        return new String(res, 0, i);
     }
 }
