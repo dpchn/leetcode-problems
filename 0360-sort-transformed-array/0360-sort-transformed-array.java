387fe4cc8d71d82f91025291d036266d2e6fa270
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-03-09 16:53:28 +0530

    Time: 1 ms (92.22%), Space: 44.4 MB (77.84%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.java
+++ 0360-sort-transformed-array/0360-sort-transformed-array.java
@@ -1,10 +1,49 @@
 class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-        for(int i=0; i< nums.length; i++){
-            nums[i] = a*nums[i]*nums[i] + b*nums[i]+c;
-        }
-        Arrays.sort(nums);
+        // for(int i=0; i< nums.length; i++){
+        //     nums[i] = a*nums[i]*nums[i] + b*nums[i]+c;
+        // }
+        // Arrays.sort(nums);
 
-        return nums;
+        // return nums;
+
+        int len = nums.length;
+        int right=len-1, left=0;
+        int result[] = new int[len];
+        if(a >=0){
+            int index=len-1;
+            while(left <=right){
+                int leftVal = transform(nums[left], a,b,c);
+                int rightVal = transform(nums[right], a,b,c);
+                if(leftVal< rightVal){
+                    result[index] = rightVal;
+                    right--;
+                }else{
+                    result[index] = leftVal;
+                    left++;
+                }
+                index--;
+            }
+        }else{
+            int index=0;
+            while(left <=right){
+                int leftVal = transform(nums[left], a,b,c);
+                int rightVal = transform(nums[right], a,b,c);
+                if(leftVal> rightVal){
+                    result[index] = rightVal;
+                    right--;
+                }else{
+                    result[index] = leftVal;
+                    left++;
+                }
+                index++;
+            }
+        }
+        return result;
+    }
+
+
+    int transform(int num, int a, int b, int c){
+        return a*num*num + b*num+c;
     }
 }
