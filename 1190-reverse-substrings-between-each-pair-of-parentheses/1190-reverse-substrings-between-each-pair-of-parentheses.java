b1a98f404a68d5e66dcaef80e9cbe30f3098e7d0
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-03-19 18:04:46 +0530

    Time: 26 ms (14.54%), Space: 47.2 MB (25.29%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,68 +1,31 @@
 class Solution {
-    // public String reverseParentheses(String s) {
-    //     List<Character> stack = new ArrayList<Character>();
-
-    //     for(int i=s.length()-1; i>=0; i--){
-    //         char ch = s.charAt(i);
-    //         if(ch=='('){
-    //             StringBuilder sb = new StringBuilder();
-    //             while(!stack.isEmpty() && stack.get(stack.size()-1)!=')'){
-    //                 sb.append(stack.remove(stack.size()-1));
-    //             }
-    //             stack.remove(stack.size()-1);
-    //             String reverse = sb.reverse().toString();
-    //             for(char c : reverse.toCharArray()){
-    //                 stack.add(c);
-    //             }
-    //         }else{
-    //             stack.add(ch);
-    //         }
-    //     }
-
-    //     StringBuilder sb = new StringBuilder();
-    //     for(char c : stack){
-    //         sb.append(c);
-    //     }
-
-    //     return sb.toString();
- 
-    // }
-
-
     public String reverseParentheses(String s) {
-    Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<Character>();
 
-    for (char ch : s.toCharArray()) {
-        if (ch == ')') {
-            // 1. Pop characters until we hit '('
-            StringBuilder temp = new StringBuilder();
-            while (!stack.isEmpty() && stack.peek() != '(') {
-                temp.append(stack.pop());
-            }
-            
-            // 2. Remove the '(' from stack
-            if (!stack.isEmpty()) {
-                stack.pop();
-            }
+        for(char ch : s.toCharArray()){
+            if(ch==')'){
+                StringBuilder sb = new StringBuilder();
+                while(!stack.isEmpty() && stack.peek()!='('){
+                    sb.append(stack.pop());
+                }
 
-            // 3. Add the reversed characters back to the stack
-            // Note: temp is already reversed because of how we popped from stack
-            for (int i = 0; i < temp.length(); i++) {
-                stack.push(temp.charAt(i));
+                if(!stack.isEmpty())
+                    stack.pop();
+
+                for(int i=0; i< sb.length(); i++){
+                    stack.push(sb.charAt(i));
+                }
+            }else{
+                stack.push(ch);
             }
-        } else {
-            // Push letters and '(' onto the stack
-            stack.push(ch);
         }
-    }
 
-    // Build final result from stack
-    StringBuilder result = new StringBuilder();
-    for (char ch : stack) {
-        result.append(ch);
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack){
+            sb.append(c);
+        }
+        return sb.toString();
     }
-    return result.toString();
-}
 }
 
 
