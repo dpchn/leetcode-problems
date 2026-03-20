c6f28a9cbac6b6a31385152776f771665b68640c
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-03-20 10:32:51 +0530

    Time: 1 ms (100%), Space: 42.7 MB (98.24%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,28 +1,60 @@
 class Solution {
-    public String reverseParentheses(String s) {
-        Stack<Character> stack = new Stack<Character>();
+    // public String reverseParentheses(String s) {
+    //     Stack<Character> stack = new Stack<Character>();
 
-        for(char ch : s.toCharArray()){
+    //     for(char ch : s.toCharArray()){
+    //         if(ch==')'){
+    //             StringBuilder sb = new StringBuilder();
+    //             while(!stack.isEmpty() && stack.peek()!='('){
+    //                 sb.append(stack.pop());
+    //             }
+
+    //             if(!stack.isEmpty())
+    //                 stack.pop();
+
+    //             for(int i=0; i< sb.length(); i++){
+    //                 stack.push(sb.charAt(i));
+    //             }
+    //         }else{
+    //             stack.push(ch);
+    //         }
+    //     }
+
+    //     StringBuilder sb = new StringBuilder();
+    //     for(char c : stack){
+    //         sb.append(c);
+    //     }
+    //     return sb.toString();
+    // }
+
+
+
+        public String reverseParentheses(String s) {
+        int len = s.length();
+        int pair[] = new int[len];
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i=0; i< len ; i++){
+            char ch = s.charAt(i);
             if(ch==')'){
-                StringBuilder sb = new StringBuilder();
-                while(!stack.isEmpty() && stack.peek()!='('){
-                    sb.append(stack.pop());
-                }
-
-                if(!stack.isEmpty())
-                    stack.pop();
-
-                for(int i=0; i< sb.length(); i++){
-                    stack.push(sb.charAt(i));
-                }
-            }else{
-                stack.push(ch);
+                int indx = stack.pop();
+                pair[i]= indx;
+                pair[indx]= i;
+            }else if(ch=='('){
+                stack.push(i);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(char c : stack){
-            sb.append(c);
+        int d=1;
+        for(int i=0; i< len; i= i+d){
+            char ch = s.charAt(i);
+            if(ch==')' || ch=='('){
+                i = pair[i];
+                d = -d;
+            }else{
+                sb.append(ch);
+            }
         }
         return sb.toString();
     }
