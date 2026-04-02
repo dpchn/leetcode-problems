48662a480b2ef3b642b6b1680c8810569680ac68
Durga Prasad Chauhan <durgaprasadchncse@gmail.com>
2026-04-02 09:55:54 +0530

    Time: 1 ms (59.07%), Space: 47.4 MB (93.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,37 +1,63 @@
 class Solution {
     public int trap(int[] height) {
-        int maxR[] = findMaxSoFarR(height);
-        int maxL[] = findMaxSoFarL(height);
-        int total =0;
-        for(int i=0; i< height.length; i++){
-            int water = Math.min(maxR[i], maxL[i]) - height[i];
-            total = total + water;
-        }
-        return total;
-    }
+        // int sum =0;
+        // int left[] =leftMax(height);
+        // int right[] = rightMax(height);
+        // for(int i=0; i< height.length; i++){
+        //     sum = sum + Math.min(left[i], right[i]) - height[i];
+        // }
+        // return sum;
 
-    int[] findMaxSoFarR(int arr[]){
-        int len = arr.length;
-        int res[] = new int[len];
-        int c = len-1;
-        int maxSoFar = arr[len-1];
-        for(int i = len-1; i>=0; i--){
-            maxSoFar = Math.max(maxSoFar, arr[i]);
-            res[c--] = maxSoFar;
-        }
-        return res;
+        return optimizeSolution(height);
+        
     }
 
 
-    int[] findMaxSoFarL(int arr[]){
-        int len = arr.length;
-        int res[] = new int[len];
-        int c = 0;
-        int maxSoFar = arr[0];
-        for(int i = 0; i<len; i++){
-            maxSoFar = Math.max(maxSoFar, arr[i]);
-            res[i] = maxSoFar;
+    int optimizeSolution(int[] height){
+        int left=0;
+        int leftMax=0;
+        int right = height.length-1;
+        int rightMax=0;
+        int sum=0;
+
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left]> leftMax){
+                    leftMax = height[left];
+                }else{
+                    sum = sum + leftMax - height[left];
+                }
+                left++;
+            }else{
+                if(height[right]> rightMax){
+                    rightMax = height[right];
+                }else{
+                    sum = sum + rightMax - height[right];
+                }
+                right--;
+            }
         }
-        return res;
+        return sum;
+    }
+
+
+    int[] leftMax(int height[]){
+        int maxSoFar=0, i=0;
+        int result[] = new int[height.length];
+        for(int n : height){
+            maxSoFar = Math.max(maxSoFar, n);
+            result[i++] = maxSoFar;
+        }
+        return result;
+    }
+
+    int[] rightMax(int height[]){
+        int maxSoFar=0;
+        int result[] = new int[height.length];
+        for(int i=height.length-1; i>=0; i--){
+            maxSoFar = Math.max(maxSoFar, height[i]);
+            result[i] = maxSoFar;
+        }
+        return result;
     }
 }
